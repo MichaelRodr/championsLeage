38e955538bd74791f0e07d548bf2e551ed66f811
MichaelRodr <cequea.mr@gmail.com>
2023-02-05 15:30:01 -0300

    click button

--- champions.py
+++ champions.py
@@ -1,15 +1,29 @@
 from selenium import webdriver
 import time
+import openpyxl
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 
+
+# Libreria Chrome
 driver = webdriver.Chrome(executable_path=r'.\chomedriver.exe')
 
 # Navigate to
 driver.get("https://www.google.com.br")
 inputElement = driver.find_element(By.CLASS_NAME, "gLFyf")
 time.sleep(4)
-inputElement.send_keys('resultados de champios leage'+ Keys.ENTER)
-time.sleep(20)
+inputElement.send_keys('resultados de champios leage tabla'+ Keys.ENTER)
+time.sleep(4)
+
+# Obtain Data
+button = driver.find_element(By.CLASS_NAME,"U0faLd CNf3nf OvQkSb LhCR5d")
+button.click()
+time.sleep(60)
+
+
+
+
+
 
