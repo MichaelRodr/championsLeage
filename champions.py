c8e6bfda27d8a21cf6cd457bc07c6d4a660a536e
MichaelRodr <cequea.mr@gmail.com>
2023-02-05 11:03:06 -0300

    search result

--- champions.py
+++ champions.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Chrome(executable_path=r'.\chomedriver.exe')
 
-driver.get("https://www.google.com")
-inputElement =driver.find_element("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input").click()
+# Navigate to
+driver.get("https://www.google.com.br")
+inputElement = driver.find_element(By.CLASS_NAME, "gLFyf")
 time.sleep(4)
+inputElement.send_keys('resultados de champios leage'+ Keys.ENTER)
+time.sleep(20)
 
